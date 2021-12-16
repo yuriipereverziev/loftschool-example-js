1614bf087f47f47557d65b9b2027fedda75dd492
yuriipereverziev <yuripereverzev@gmail.com>
2021-12-16 21:02:54 +0200

    done 4 homeworks

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {
+function returnFirstArgument(name) {
+    return name
 }
 
 /*
@@ -30,6 +31,9 @@ function returnFirstArgument() {
    sumWithDefaults(10) вернет 110
  */
 function sumWithDefaults(a, b) {
+    b = b || 100;
+
+    return a + b
 }
 
 /*
@@ -41,6 +45,7 @@ function sumWithDefaults(a, b) {
    returnFnResult(() => 'привет') вернет 'привет'
  */
 function returnFnResult(fn) {
+    return fn()
 }
 
 /*
@@ -57,6 +62,11 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 13
  */
 function returnCounter(number) {
+    number = number || 0;
+
+    return function F() {
+        return ++ number
+    }
 }
 
 /*
